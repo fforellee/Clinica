a0a217ddfb55db1c7afd3eb032ccbd3cb9f87558
fforelle <xdollboyx@gmail.com>
2021-05-21 18:00:00 -0300

    gitignore

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 7000;
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var query = { address: "Park Lane 38" };
+  dbo.collection("customers").find(query).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+}); 
 
 app.use(express.urlencoded());
 app.use(express.json());
