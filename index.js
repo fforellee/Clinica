8a390439c087c6a3a712c1b7169ba19cbca485e7
fforelle <xdollboyx@gmail.com>
2021-05-21 12:07:06 -0300

    nodejs

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 7000;
-const server = app.listen(port, ()=>{
 
-    console.log('Express running -> PORT${server.adress().port}');
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.get('/index',(req,res)=>{
+    res.sendFile(__dirname+'/root/index.html')
+});
+
+app.post('/form',(req,res)=>{
+    console.log(req.body.user.name)
+    console.log(req.body.user.email)
+});
+            
+const server = app.listen(port, ()=>{
+    console.log('running server at '+port);
 });
